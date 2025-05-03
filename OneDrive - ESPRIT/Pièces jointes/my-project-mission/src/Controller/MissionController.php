e12d212ce5602afc589477a9bba13fcd9336e9e7
ChirazWesleti <101870941+ChirazWesleti@users.noreply.github.com>
2025-05-03 10:46:30 +0100

    lastttttt

--- OneDrive - ESPRIT/Pièces jointes/my-project-mission/src/Controller/MissionController.php
+++ OneDrive - ESPRIT/Pièces jointes/my-project-mission/src/Controller/MissionController.php
@@ -35,7 +35,6 @@ class MissionController extends AbstractController
             $entityManager->flush();
 
             $this->addFlash('success', 'Mission créée avec succès');
-            return $this->redirectToRoute('app_mission_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('mission/new.html.twig', [
